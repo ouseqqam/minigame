92cfc39f6624fd23fe611c49202080a2d66b0020
Oussama Seqqam <ouseqqam@e2r10p1.1337.ma>
2021-06-01 19:02:29 +0100

    main

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-x = document.getElementById('1').addEventListener(onclick,myImg());
-function myImg(){
-    x.src = 'img/banane.jpg';
+let tab = ["img/sky.jpg","img/banane.jpg", "img/download.jpg", "img/peach.jpg"];
+var img = [];
+var id1 = -1;
+var i = -1;
+
+function myImg(id) {
+    if (id == 0 || id == 4)
+    {
+        if (i == -1 || i == 0)
+        {
+            document.getElementById(id).src = tab[3];
+            i = 0;
+        }
+        else
+        {
+            document.getElementById(id1).src = tab[0];
+            document.getElementById(id).src = tab[0];
+        }
+        id1 = id;
+    }
 }
